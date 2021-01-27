6a62671711ca768b8d641eeb503e102d70ae5506
Matheus Xavier <mattheus.x.a@gmail.com>
2021-01-27 00:30:40 -0300

    associations

--- backend/src/models/recipeModel.ts
+++ backend/src/models/recipeModel.ts
@@ -1,2 +1,32 @@
 import Sequelize from 'sequelize'
 import { Recipe } from './recipe'
+import database from '../database'
+
+const RecipeModel = database.define('recipe', {
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    categories: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    time: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    photos: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        
+    },
+
+
+
+})
